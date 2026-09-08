e57aa97f9b69ce079b45e275e099ec0b68d31bb3
Murilo Wilbert <murilowilbert@gmail.com>
2026-09-08 14:05:40 -0300

    Novos Arquivos

--- Avaliacao/App.java
+++ Avaliacao/App.java
@@ -42,11 +42,12 @@ public class App {
         while (true) {
             System.out.println("\n1 - Cadastrar novo jogo");
             System.out.println("2 - Listar jogos cadastrados");
-            System.out.println("3 - Sair");
+            System.out.println("3 - Atualizar cadastro");
+            System.out.println("4 - Sair");
 
             while (true) {
                 escolha = lerInt(scanner, "Escolha uma opção: ", "Erro! Digite um número inteiro.");
-                if (escolha >= 1 && escolha <= 3) {
+                if (escolha >= 1 && escolha <= 4) {
                     break;
                 } else {
                     System.out.printf("A opção (%d) não existe! Tente novamente.\n", escolha);
@@ -103,6 +104,78 @@ public class App {
                 }
 
                 case 3: {
+                    boolean temJogos = false;
+                    for (int i = 0; i < listaJogos.length; i++) {
+                        if (listaJogos[i] != null) {
+                            System.out.printf("\n%d - %s", i + 1, listaJogos[i].getTitulo());
+                            temJogos = true;
+                        }
+                    }
+                    System.out.println();
+                    if (!temJogos) {
+                        System.out.println("Nenhum jogo cadastrado!");
+                        break;
+                    }
+                    while (true) {
+                        escolha = lerInt(scanner, "Qual cadastro deseja alterar (índice): ",
+                                "Erro! Digite um número inteiro.");
+                        escolha--;
+                        if (escolha >= 0 && escolha < listaJogos.length && listaJogos[escolha] != null) {
+                            System.out.println("Título: " + listaJogos[escolha].getTitulo());
+                            String titulo = lerString(scanner, "Atualizar (digite '-1' para manter): ",
+                                    "Erro! Digite um titulo válido.");
+                            System.out.println("=======================");
+                            System.out.println("Descrição: " + listaJogos[escolha].getDescricao());
+                            String descricao = lerString(scanner, "Atualizar (digite '-1' para manter): ",
+                                    "Erro! Digite uma descrição válida.");
+                            System.out.println("=======================");
+                            System.out.println("Ano de lançamento: " + listaJogos[escolha].getAnoLancamento());
+                            int anoLancamento = lerInt(scanner, "Atualizar (digite '-1' para manter): ",
+                                    "Erro! Digite um número inteiro");
+                            System.out.println("=======================");
+                            System.out.println("Nota: " + listaJogos[escolha].getAvaliacao().getNota());
+                            while (true) {
+                                nota = lerInt(scanner, "Atualizar (digite '-1' para manter): ",
+                                        "Erro! Digite um número inteiro");
+                                if (nota >= 0 && nota <= 5 || nota == -1) {
+                                    break;
+                                } else {
+                                    System.out.println("Nota inválida! Digite uma nota entre 0 à 5.");
+                                }
+                            }
+                            System.out.println("=======================");
+                            System.out.println("Crítica: " + listaJogos[escolha].getAvaliacao().getCritica());
+                            String critica = lerString(scanner, "Atualizar (digite '-1' para manter): ",
+                                    "Erro! Digite uma crítica válida");
+                            System.out.println("=======================");
+                            if (!titulo.equals("-1")) {
+                                listaJogos[escolha].setTitulo(titulo);
+                            }
+
+                            if (!descricao.equals("-1")) {
+                                listaJogos[escolha].setDescricao(descricao);
+                            }
+
+                            if (anoLancamento != -1) {
+                                listaJogos[escolha].setAnoLancamento(anoLancamento);
+                            }
+
+                            if (nota != -1) {
+                                listaJogos[escolha].getAvaliacao().setNota(nota);
+                            }
+
+                            if (!critica.equals("-1")) {
+                                listaJogos[escolha].getAvaliacao().setCritica(critica);
+                            }
+                            System.out.println("Jogo atualizado com sucesso!");
+                            break;
+                        } else {
+                            System.out.println("Opção inválida! Escolha um dos índices listados.");
+                        }
+                    }
+                }
+
+                case 4: {
                     System.out.println("Saindo do sistema...");
                     scanner.close();
                     return;
